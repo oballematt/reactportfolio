ed1edcd791f1d39f62a61c672ab0086c0d3b3a30
Matthew Oballe <oballematt@gmail.com>
2020-11-26 00:54:42 -0600

    updated styling and added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Portfolio from "./pages/Portfolio"
+import Nav from "../src/components/navbar/index"
+import Footer from "../src/components/footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+        <Switch>
+          <Route exact path="/reactportfolio" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </Switch>
+      <Footer />
+    </Router>
   );
 }
 
