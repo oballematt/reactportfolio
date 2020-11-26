77a0571b927a9528aba6b8a4d6747bbecde51468
Matthew Oballe <oballematt@gmail.com>
2020-11-26 01:01:48 -0600

    fixing syntax

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -31,20 +31,20 @@ export default function Contact() {
             <form className="contact-form" onSubmit={sendEmail}>
                 <div className="form-row">
                     <div className="form-group col-md-6">
-                        <label for="firstName">First Name</label>
+                        <label htmlFor="firstName">First Name</label>
                         <input type="text" className="form-control" name="from_name" placeholder="First Name" />
                     </div>
                     <div className="form-group col-md-6">
-                        <label for="lastName">Last Name</label>
+                        <label htmlFor="lastName">Last Name</label>
                         <input type="text" className="form-control" name="last_name" placeholder="Last Name" />
                     </div>
                 </div>
                 <div className="form-group">
-                    <label for="inputEmail4">Email</label>
+                    <label htmlFor="inputEmail4">Email</label>
                     <input type="email" className="form-control" name="email_name" id="inputEmail4" placeholder="Email" />
                 </div>
                 <div className="form-group">
-                    <label for="exampleFormControlTextarea1">Comments</label>
+                    <label htmlFor="exampleFormControlTextarea1">Comments</label>
                     <textarea className="form-control" name="message" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
                 <button value="send" type="submit" className="btn btn-light opacity">Email Me</button>
