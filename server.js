9db93ba1834f1ef39b2632af3cdc969691a8d9de
Matthew Oballe <oballematt@gmail.com>
2020-12-04 22:39:40 -0600

    updating server

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-const express = require("express");
-
+const path = require('path');
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+const publicPath = path.join(__dirname, '.', 'public');
+const port = process.env.PORT || 3001;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+   console.log('Server is up!');
 });
